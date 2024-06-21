6d2832666c2558e6b442cbe79ebfda815d06ab54
WindowsLogic <admin@windowslogic.co.uk>
2024-06-21 22:22:12 +0100

    Initial site design and information.

---
+++ index.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="The Manchester Brony meet.">
+		<meta name="keywords" content="the, mancunilings, hive, the mancunilings hive, brony, meet, brony meet, 16+ brony meet, fun, games, friendly">
+		<title>The Mancunilings</title>
+		<link rel="stylesheet" href="css/style.css"/>
+		<link rel="shortcut icon" href="img/simp.png" type="image/x-icon">
+		<meta property="og:title" content="Mancunilings"/>
+		<meta property="og:type" content="website"/>
+		<meta property="og:url" content="https://mancunilings.org.uk"/>
+		<meta property="og:image" content="img/simp.png">
+		<meta property="og:description" content="The Manchester Brony meet."/>
+		<meta property="og:determiner" content="the"/>
+	</head>
+
+<section class="placeholder">
+<header>
+	<?php
+    include('partials/header.php');
+    ?>
+
+</header>
+</section>
+	
+	
+			<section class="placeholder">
+                
+                <section id="box">
+<!--Start of the content section.-->
+<section class="info3">
+<center>
+<br>
+<br>
+<h3>Every 4th Saturday
+<h2 id="header">The Manchester Pony Meetup</h2>
+<h3 style='color:black;'>at Moon Under Water (Wetherspoons)</h3>
+<br>
+</center>
+</section>
+
+<section class="info2">
+<center>
+<h2>A day of friends, fun and games.</h2>
+<p>Mancunilings is the social group for bronies in and around Manchester to meet monthly.</p>
+<a href="characters.php"><button class="buttoncenter button1">Join The Discord Server</button></a>
+<br>
+<br>
+</center>
+</section>
+
+<section class="info">
+<center>
+<h2 style='color:white;'>Meet Details</h2>
+<p style='color:white;'>From 11:00, the pre-meet will begin in Manchester Piccadilly Starbucks (Mezzanine level on the main concourse), look out for our crochet cutie plush Misty!</p>
+<p style='color:white;'>At 12:00, we will walk through the city to The Moon Under Water (Wetherspoons) where we will stay until 17:00 when the meet officially ends. We are, however likely to make a detour to get some bubble tea, so if you can, please join us for that!</p>
+<br>
+</center>
+</section>
+
+<section class="info2">
+<center>
+<h2>Our Mascot (Name Undecided)</h2>
+<img src="img/Coloured_changey_boi.png"></img>
+<p>This is our mascot for The Mancunilings meetups, the design is based on the Changeling (MLP), this design was chosen to fit the theme of a bee, as the main symbol for Manchester is the worker bee.</p>
+<br>
+<br>
+</center>
+</section>
+
+<section class="info">
+	<center>
+	<h2></h2>
+<div class="row">
+  <div class="column">
+<img src="img/ss1.png" width="90%" class="image-transition-dev"></img>
+</div>
+  <div class="column">
+<img src="img/ss5.png" width="90%" class="image-transition-dev"></img>
+</div>
+<div class="column">
+<img src="img/ss3.png" width="90%" class="image-transition-dev"></img>
+</div>
+<div class="row">
+  <div class="column">
+<img src="img/ss2.png" width="90%" class="image-transition-dev"></img>
+</div>
+  <div class="column">
+<img src="img/ss4.png" width="90%" class="image-transition-dev"></img>
+</div>
+<div class="column">
+<img src="img/ss6.png" width="90%" class="image-transition-dev"></img>
+</div>
+<br>
+<br>
+<br>
+</center>
+</section>
+
+          
+            <footer>
+            <?php
+    include('partials/footer.php');
+    ?>
+            </footer>
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,7 @@
+<div id="text"><a title="Mancunilings Home" href="index.php"><img src="img/simp.png" width="auto" height="38"/>Mancunilings</a></div>
+		<nav id="nav">
+            <ul id="navbar">
+				<li><a title="See what we've been up to during the past meets." href="meet-reports.php">Meet Reports</a></li>
+			</ul>
+        </nav>
+
