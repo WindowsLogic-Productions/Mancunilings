0766c47775d6143961004383d5b69d9e2e7e5d18
WindowsLogic <admin@windowslogic.co.uk>
2024-06-22 22:28:14 +0100

    Test footer year.

--- partials/footer.php
+++ partials/footer.php
@@ -1,10 +1,18 @@
 ﻿<class id="footertext">
 <br>
+			
 			<a href="https://windowslogic.co.uk/"><img title="Software and services made with passion." src="img/footerbanner.png"></img></a>
-			<p style='color:white;'>Copyright © 2011 - 2024 WindowsLogic Productions. All rights reserved.</p>
+			<p style='color:white;'>Copyright © 2011 - <div id="currentyear"><script>
+			date = new Date();
+			year = date.getFullYear();
+			month = date.getMonth() + 1;
+			day = date.getDate();
+			document.getElementById("currentyear").innerHTML = year;
+			</script></div> WindowsLogic Productions. All rights reserved.</p>
 			<p style='color:white;'>The Mancunilings Brony meetups uses this site under license from WindowsLogic Productions.</p>
 			<p style='color:white;'>All other trademarks and copyrights are the property of their respective owners. All rights reserved.</p>
 			</class>
 			<class id="footerend">
 			<a href="https://www.paypal.com/donate?hosted_button_id=8TE9GD6PAGB8Q">Donate</a> / <a href="https://windowslogic.co.uk/privacy-policy.php">Privacy Policy</a> / <a href="https://windowslogic.co.uk/terms.php">Terms of Service</a>
 			</class>
+			
