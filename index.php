650f798494b6608bb2462e05c6d5ac2519ef9443
WindowsLogic <admin@windowslogic.co.uk>
2024-06-22 14:14:00 +0100

    Site improvements.
    - Added new section to the index for meet reports.
    - Added meet reports page.
    - Fixed navbar colours.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="description" content="The Manchester Brony meet.">
-		<meta name="keywords" content="the, mancunilings, hive, the mancunilings hive, brony, meet, brony meet, 16+ brony meet, fun, games, friendly">
+		<meta name="keywords" content="the, mancunilings, hive, the mancunilings hive, brony, meet, brony meet, 16+ brony meet, fun, games, friendly, manchester brony meet, manchester">
 		<title>The Mancunilings</title>
 		<link rel="stylesheet" href="css/style.css"/>
 		<link rel="shortcut icon" href="img/pic.png" type="image/x-icon">
@@ -55,7 +55,6 @@
 <h2 style='color:white;'>Meet Details</h2>
 <p style='color:white;'>From 11:00, the pre-meet will begin in Manchester Piccadilly Starbucks (Mezzanine level on the main concourse), look out for our crochet cutie plush Misty!</p>
 <p style='color:white;'>At 12:00, we will walk through the city to The Moon Under Water (Wetherspoons) where we will stay until 17:00 when the meet officially ends. We are, however likely to make a detour to get some bubble tea, so if you can, please join us for that!</p>
-<br>
 </center>
 </section>
 
@@ -64,14 +63,23 @@
 <h2>Our Mascot (Name Undecided)</h2>
 <img src="img/Coloured_changey_boi.png"></img>
 <p>This is our mascot for The Mancunilings meetups, the design is based on the Changeling (MLP), this design was chosen to fit the theme of a bee, as the main symbol for Manchester is the worker bee.</p>
+</center>
+</section>
+
+<section class="info">
+<center>
+<h2 style='color:white;'>Meet Reports</h2>
+<p style='color:white;'>Every time there's a meet, we write a meet report to reflect on what happened at that meet. This allows us to look back and remember. Click the button below to head on over to the meet reports blog.</p>
+<a href="meet-reports.php"><button class="buttoncenter button1">See Our Meet Reports</button></a>
 <br>
 <br>
 </center>
 </section>
 
-<section class="info">
+<section class="info2">
 	<center>
-	<h2></h2>
+	<h2>Our Meet Gallery</h2>
+	<p>See a selection of photos from previous meets to get a feel for what we're all about.</p>
 <div class="row">
   <div class="column">
 <img src="img/ss1.png" width="90%" class="image-transition-dev"></img>
---
+++ meet-reports.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="The Manchester Brony meet.">
+		<meta name="keywords" content="the, mancunilings, hive, the mancunilings hive, brony, meet, brony meet, 16+ brony meet, fun, games, friendly, manchester brony meet, manchester">
+		<title>The Mancunilings - Meet Reports</title>
+		<link rel="stylesheet" href="css/style.css"/>
+		<link rel="shortcut icon" href="img/pic.png" type="image/x-icon">
+		<meta property="og:title" content="Mancunilings - Meet Reports"/>
+		<meta property="og:type" content="website"/>
+		<meta property="og:url" content="https://mancunilings.org.uk"/>
+		<meta property="og:image" content="img/pic.png">
+		<meta property="og:description" content="The Manchester Brony meet."/>
+		<meta property="og:determiner" content="the"/>
+	</head>
+
+<section class="placeholder">
+<header>
+	<?php
+    include('partials/header.php');
+    ?>
+
+</header>
+</section>
+	
+	
+			<section class="placeholder">
+                
+                <section id="box">
+<!--Start of the content section.-->
+<section class="info3">
+<center>
+<br>
+<br>
+<h2 id="header">Meet Reports</h2>
+<h3 style='color:black;'>See what we've been up to.</h3>
+<br>
+</center>
+</section>
+
+<section class="info2">
+<center>
+<h2>Meet Report - 22nd June 2024</h2>
+<p>Coming soon...</p>
+<br>
+<br>
+</center>
+</section>
+
+<!--<section class="info">
+<center>
+<h2 style='color:white;'>Meet Details</h2>
+<p style='color:white;'>From 11:00, the pre-meet will begin in Manchester Piccadilly Starbucks (Mezzanine level on the main concourse), look out for our crochet cutie plush Misty!</p>
+<p style='color:white;'>At 12:00, we will walk through the city to The Moon Under Water (Wetherspoons) where we will stay until 17:00 when the meet officially ends. We are, however likely to make a detour to get some bubble tea, so if you can, please join us for that!</p>
+<br>
+</center>
+</section>-->
+
+          
+            <footer>
+            <?php
+    include('partials/footer.php');
+    ?>
+            </footer>
+</body>
+</html>
